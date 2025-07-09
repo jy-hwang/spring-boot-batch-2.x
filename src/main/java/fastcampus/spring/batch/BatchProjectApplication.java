a88af90390aa73ed430e941845a3b7467a007098
jyhwang <stratojy@gmail.com>
2025-07-09 23:58:30 +0900

    feat. helloJob 만들어보기
    1. helloJob - helloStep 을 만들고
    2.  'helloJob started' 로그가 찍히는 지 확인

--- src/main/java/fastcampus/spring/batch/BatchProjectApplication.java
+++ src/main/java/fastcampus/spring/batch/BatchProjectApplication.java
@@ -1,9 +1,11 @@
 package fastcampus.spring.batch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class BatchProjectApplication {
 
   public static void main(String[] args) {
