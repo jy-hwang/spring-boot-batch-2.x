d6b1cf58ccd9902f7d04e4db08873c20af77b35f
jyhwang <stratojy@gmail.com>
2025-07-09 23:55:37 +0900

    feat. start project
    1. spring boot 2.7.18
    2. jdk 17
    3. spring batch

---
+++ src/test/java/fastcampus/spring/batch/BatchProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package fastcampus.spring.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
