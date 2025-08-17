dc0a8a3e03bb8f1af7efce4572e67af3d47dcee7
jyhwang <stratojy@gmail.com>
2025-08-18 01:02:59 +0900

    feat. JobExecutionListener 사용해보기
    1. afterJob() 메서드로 대상 회원 데이터 로그와 실행 시간 측정

--- src/main/java/fastcampus/spring/batch/part4/UserRepository.java
+++ src/main/java/fastcampus/spring/batch/part4/UserRepository.java
@@ -10,5 +10,5 @@ import java.util.Collection;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-  Collection<Object> findAllByUpdatedDate(LocalDate updatedDate);
+  Collection<UserEntity> findAllByUpdatedDate(LocalDate updatedDate);
 }
