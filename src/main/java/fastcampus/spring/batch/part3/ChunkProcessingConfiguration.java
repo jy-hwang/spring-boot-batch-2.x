ba117d6e6c739788d0278d5c5be763dc9f6647d3
jy-hwang <stratojy@gmail.com>
2025-08-17 16:39:40 +0900

    feat. Spring EL 로 parameter 접근하기
    1. @Value("#{jobParameter[key]}")

--- src/main/java/fastcampus/spring/batch/part3/ChunkProcessingConfiguration.java
+++ src/main/java/fastcampus/spring/batch/part3/ChunkProcessingConfiguration.java
@@ -2,8 +2,12 @@ package fastcampus.spring.batch.part3;
 
 import io.micrometer.core.instrument.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.*;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -12,6 +16,7 @@ import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -36,15 +41,16 @@ public class ChunkProcessingConfiguration {
         .get("chunkProcessingJob")
         .incrementer(new RunIdIncrementer())
         .start(this.taskBaseStep())
-        .next(this.chunkBaseStep())
+        .next(this.chunkBaseStep(null))
         .build();
   }
 
   @Bean
-  public Step chunkBaseStep() {
+  @JobScope
+  public Step chunkBaseStep(@Value("#{jobParameters[chunkSize]}") String chunkSize) {
     return stepBuilderFactory
         .get("chunkBaseStep")
-        .<String, String>chunk(10)
+        .<String, String>chunk(StringUtils.isNotEmpty(chunkSize) ? Integer.parseInt(chunkSize) : 10)
         .reader(itemReader())
         .processor(itemProcessor())
         .writer(itemWriter())
