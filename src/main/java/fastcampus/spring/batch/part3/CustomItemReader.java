28890be8ff4a7063fd47d10f16cd4cf8fcfe9197
jyhwang <stratojy@gmail.com>
2025-08-18 00:03:05 +0900

    chore. formatting

--- src/main/java/fastcampus/spring/batch/part3/CustomItemReader.java
+++ src/main/java/fastcampus/spring/batch/part3/CustomItemReader.java
@@ -1,9 +1,6 @@
 package fastcampus.spring.batch.part3;
 
 import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.NonTransientResourceException;
-import org.springframework.batch.item.ParseException;
-import org.springframework.batch.item.UnexpectedInputException;
 
 import java.util.ArrayList;
 import java.util.List;
